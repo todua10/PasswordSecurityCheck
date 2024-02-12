a2e02ad0a0b567bf4aa6d89d5e31162f3baaa1d8
todua10 <todua10@inbox.ru>
2024-02-12 18:25:54 +0500

    Adding CSS styles, application icons, transferring the show/hide text button to the text field, adding hints.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,35 +2,41 @@ package com.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.util.Objects;
 public class Main extends Application{
     public static void main(String[] args) {
-        System.out.println("Launching Application");
+        System.out.println("Запуск приложения");
         launch(args);
     }
     @Override
     public void init() throws Exception {
-        System.out.println("Application init");
+        System.out.println("Инициализация приложения");
         super.init();
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Main.fxml")));
-        Scene scene = new Scene(root, 500, 520, Color.WHITE);
+        primaryStage.getIcons().add(new Image(Objects
+                .requireNonNull(getClass()
+                .getResourceAsStream("/icon.png"))));
+        Parent root = FXMLLoader.load(Objects
+                .requireNonNull(getClass()
+                .getResource("/Main.fxml")));
+        Scene scene = new Scene(root, 600, 250, Color.WHITE);
         primaryStage.setScene(scene);
         primaryStage.centerOnScreen();
         primaryStage.setResizable(true);
-        primaryStage.setMinHeight(320);
-        primaryStage.setMinWidth(300);
+        primaryStage.setMinHeight(250);
+        primaryStage.setMinWidth(600);
         primaryStage.setTitle("Проверка надёжности пароля");
         primaryStage.sizeToScene();
         primaryStage.show();
     }
     @Override
     public void stop() throws Exception {
-        System.out.println("Application stops");
+        System.out.println("Остановка приложения");
         super.stop();
     }
 }
--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -4,9 +4,10 @@ import javafx.scene.Group;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.*;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 import java.util.Objects;
-
 public class MainController {
     private final ImageView imageShow =
             new ImageView(Objects.requireNonNull(getClass().getResource("/show.png")).toExternalForm());
@@ -29,8 +30,22 @@ public class MainController {
     private TextField passwordFieldText;
     @FXML
     private TextField password2FieldText;
-    @FXML
-    private Label labelOut;
+    private final Tooltip tooltipStrong = new Tooltip("Надёжный пароль!");
+    private final Tooltip tooltipWeak = new Tooltip("Слабый пароль!");
+    private final Tooltip tooltipMismatch = new Tooltip("Пароли не совпадают!");
+    private final Tooltip tooltipPass = new Tooltip("""
+            Пароль должен быть не менее 8 символов и содержать:\s
+             - Одну заглавную букву(русскую или английскую);\s
+             - Одну строчную букву(русскую или английскую);\s
+             - Одну цифру;\s
+             - Один специальный символ(@#$%^&+=).""");
+    PauseTransition pause = new PauseTransition(Duration.seconds(2.5));
+
+    private void setParams4ToolTip(Tooltip tt, double showDelay, double hideDelay) {
+        tt.setShowDelay(Duration.millis(showDelay));
+        tt.setHideDelay(Duration.seconds(hideDelay));
+    }
+
     @FXML
     private void check() {
         String pass;
@@ -43,17 +58,30 @@ public class MainController {
             pass2 = password2FieldText.getText();
         }
         if (!pass.equals(pass2)){
-            labelOut.setText("Пароли не совпадают!");
+            checkPassword.setTooltip(tooltipMismatch);
+            tooltipMismatch.show(checkPassword,
+                    checkPassword.localToScreen(checkPassword.getBoundsInLocal()).getMinX(),
+                    checkPassword.localToScreen(checkPassword.getBoundsInLocal()).getMaxY());
+            pause.play();
         } else if (pass.matches("^((?=.*[а-яёА-ЯЁ])|(?=.*[a-zA-Z]))(?=.*[0-9])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")){
-            labelOut.setText("Надёжный пароль!");
+            checkPassword.setTooltip(tooltipStrong);
+            tooltipStrong.show(checkPassword,
+                    checkPassword.localToScreen(checkPassword.getBoundsInLocal()).getMinX(),
+                    checkPassword.localToScreen(checkPassword.getBoundsInLocal()).getMaxY());
             Clipboard clipboard = Clipboard.getSystemClipboard();
             ClipboardContent content = new ClipboardContent();
             content.putString(pass);
             clipboard.setContent(content);
+            pause.play();
         } else {
-            labelOut.setText("Слабый пароль!");
+            checkPassword.setTooltip(tooltipWeak);
+            tooltipWeak.show(checkPassword,
+                    checkPassword.localToScreen(checkPassword.getBoundsInLocal()).getMinX(),
+                    checkPassword.localToScreen(checkPassword.getBoundsInLocal()).getMaxY());
+            pause.play();
         }
     }
+
     @FXML
     private void show(){
         if (!showPasswords){
@@ -79,7 +107,58 @@ public class MainController {
     @FXML
     public void onEnter(KeyEvent e){
         if(e.getCode() == KeyCode.ENTER) {
-            check();
+            checkPassword.fire();
         }
+    }
+    @FXML
+    public void initialize() {
+        showHidePassword.setFocusTraversable(false);
+        pause.setOnFinished(event -> {
+            tooltipMismatch.hide();
+            tooltipStrong.hide();
+            tooltipWeak.hide();
+        });
+        tooltipMismatch.setStyle("-fx-font-family: \"Century Gothic\"; "
+                + "-fx-font-size: 14; "
+                + "-fx-background-color: #ffffff; "
+                + "-fx-text-fill: #ff0000ff;");
+        tooltipStrong.setStyle("-fx-font-family: \"Century Gothic\"; "
+                + "-fx-font-size: 14; "
+                + "-fx-background-color: #ffffff; "
+                + "-fx-text-fill: #00bf00ff;");
+        tooltipWeak.setStyle("-fx-font-family: \"Century Gothic\"; "
+                + "-fx-font-size: 14; "
+                + "-fx-background-color: #ffffff; "
+                + "-fx-text-fill: #ff0000ff;");
+        tooltipPass.setStyle("-fx-font-family: \"Century Gothic\"; "
+                + "-fx-font-size: 16; "
+                + "-fx-background-color: #ffffff; "
+                + "-fx-text-fill: #00bf00ff;");
+        setParams4ToolTip(tooltipMismatch, 1, 2.5);
+        setParams4ToolTip(tooltipStrong, 1, 2.5);
+        setParams4ToolTip(tooltipWeak, 1, 2.5);
+        setParams4ToolTip(tooltipPass, 2000, 2.5);
+        passwordField.setTooltip(tooltipPass);
+        passwordFieldText.setTooltip(tooltipPass);
+        // Устанавливаем слушателя событий на изменение текста в поле passwordField
+        passwordField.textProperty().addListener((observable, oldValue, newValue) -> {
+            // При изменении текста удаляем Tooltip из кнопки
+            checkPassword.setTooltip(null);
+        });
+        // Устанавливаем слушателя событий на изменение текста в поле passwordFieldText
+        passwordFieldText.textProperty().addListener((observable, oldValue, newValue) -> {
+            // При изменении текста удаляем Tooltip из кнопки
+            checkPassword.setTooltip(null);
+        });
+        // Устанавливаем слушателя событий на изменение текста в поле password2Field
+        password2Field.textProperty().addListener((observable, oldValue, newValue) -> {
+            // При изменении текста удаляем Tooltip из кнопки
+            checkPassword.setTooltip(null);
+        });
+        // Устанавливаем слушателя событий на изменение текста в поле password2FieldText
+        password2FieldText.textProperty().addListener((observable, oldValue, newValue) -> {
+            // При изменении текста удаляем Tooltip из кнопки
+            checkPassword.setTooltip(null);
+        });
     }
 }
